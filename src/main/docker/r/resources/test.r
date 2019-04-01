fbe7e57979f2eee949d81d66c88d823c9531ab46
Thibaud Jeannin <thibaud.jeannin@gmail.com>
2019-04-01 13:47:22 +0200

    Géré commentaires dans test.r

--- src/main/docker/r/resources/test.r
+++ src/main/docker/r/resources/test.r
@@ -1,6 +1,9 @@
 dependencies <- as.character(unlist(scan('/tmp/requirements.txt', what=list(libraries=character()), quiet = TRUE)))
 
-for (i in 1:length(dependencies)) { library(dependencies[i], character.only=TRUE) }
+for (i in 1:length(dependencies)) {
+    if(grepl('#', i)){print(sprintf("Skipping %s", dep));next}
+    library(dependencies[i], character.only=TRUE)
+}
 
 library(flexioR)
 library(magrittr)
